de92a1ec27d69a667e918a6998563385603955c6
hirokii97 <basket100713hiroki.ch@gmail.com>
2024-08-18 15:32:14 +0900

    カレンダー作成

---
+++ dist/calendar.js
@@ -0,0 +1,48 @@
+"use strict";
+class Calendar {
+    constructor() {
+        let today = new Date();
+        this.year = today.getFullYear();
+        this.month = today.getMonth() + 1;
+    }
+    // 前月
+    prevMonth() {
+        if (this.month === 1) {
+            this.month = 12;
+            this.year += -1;
+        }
+        else {
+            this.month += -1;
+        }
+    }
+    // 次月
+    nextMonth() {
+        if (this.month === 12) {
+            this.month = 1;
+            this.year += 1;
+        }
+        else {
+            this.month += 1;
+        }
+    }
+}
+const date = new Calendar();
+// 表示要素
+const Month = document.getElementsByClassName('calendar__container --js')[0];
+const nextMonthButton = document.getElementsByClassName('calendar__next --js')[0];
+const prevMonthButton = document.getElementsByClassName('calendar__prev --js')[0];
+const displayMonth = () => {
+    Month.textContent = `${date.year}年 ${date.month}月`;
+};
+const init = () => {
+    displayMonth();
+};
+init();
+nextMonthButton.addEventListener('click', () => {
+    date.nextMonth();
+    displayMonth();
+});
+prevMonthButton.addEventListener('click', () => {
+    date.prevMonth();
+    displayMonth();
+});
